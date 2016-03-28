a65cb1f5c7ca9959a2fd6048a868c0fe0257bcfb
O'Connell Dylan <doconnel@haverford.edu>
2016-03-28 15:19:36 -0400

    Added next and previous question links to each question. It seems to be working well. Not sure ways the user should break it that I need to consider.

--- quizsite/quizsite/views.py
+++ quizsite/quizsite/views.py
@@ -19,25 +19,27 @@ def quizzes(request):
 # view for viewing a question. Needs both the associated quiz and question id
 def question(request, quiz_id, question_id):
 	quiz = get_object_or_404(Quiz, pk = quiz_id)
-	# we use a filter to find only the question with this id within this quiz M2M field
+	# We use a filter to find only the question with this id within this quiz M2M field
 	# Thus, if this question does not belong to this quiz, we will say no question exists
-#	question = Question.objects.get(quiz__id = quiz_id, pk = question_id ) 
 	question = get_object_or_404( Question.objects.filter( pk = question_id, quiz__id = quiz_id )) 
-	#question = get_object_or_404(Question, pk = question_id) 
 	# We use a filter to find all answers that have the requisite question_id
 	answer_list = Answer.objects.filter(question__id = question_id)
+	# We find the next and previous question using questionordering, if they exist
 	try:
 		next_question = Question.objects.get(quiz__id = quiz.id, questionordering__ordering =  ( question.questionordering_set.get(quiz_id = question.quiz_set.get().id).ordering+1))
 	except:
 		next_question = None	
-	#next_question =dQuestion.objects.get((question.questionordering_set.get(quiz.id = quiz_id).ordering +1)
-	#next_question = Question.objects.filter(quiz__id = quiz_id, questionordering__ordering = question.questionoordering__ordering + 1)
+	try:
+                prev_question = Question.objects.get(quiz__id = quiz.id, questionordering__ordering =  ( question.questionordering_set.get(quiz_id = question.quiz_set.get().id).ordering-1))
+        except:
+                prev_question = None
 	template = loader.get_template('quizsite/question.html')
 	context = {
 		'quiz' : quiz,
 		'question' : question,
 		'answer_list' : answer_list,
 		'next_question' : next_question,
+		'prev_question' : prev_question,
 	}
 	return HttpResponse(template.render(context,request))
 
